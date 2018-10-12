b2f866f4ddfbd6b458f7ba9730221c8fa35b4f53
Eslam Amin <eslamamin@MacBook-Air-1742.local>
2018-10-12 00:38:18 -0500

    Restaurant Menu

--- app.js
+++ app.js
@@ -1,51 +1,36 @@
-let menu = document.getElementById('btnFullMenu')
-let starters = document.getElementById('btnStarters')
-let entrees = document.getElementById('btnEntrees')
-let desserts = document.getElementById('btnDesserts')
-let fullmenu = document.getElementById('fullmenu')
 
-function displayDishes(plates){
-  let completed = ''
+let fullDishes = document.getElementById("fullDishes")
+let starterbutton = document.getElementByClass("starterDishes")
+let entreebutton = document.getElementByClass("entreesDishes")
+let dessertbutton = document.getElementByClass("dessertDishes")
 
-   for (i = 0; i < plates.length; i++){
-     let dishItem = `<div class="individualPlates">
-                       <img src="${plates[i].imageURL}"/>
-                       <div class="description">
-                         <h5>${plates[i].title}</h5>
-                         <p>${plates[i].description}</p>
-                       </div>
-                       <p class="price">${plates[i].price}</p>
-                     </div>`
-      completed += dishItem
-
-   }
-   fullmenu.innerHTML = completed
-
-
+function starterDishesDisplay(dishes){
+  completed = ''
+  if (course == "Starters"){
+     dishes += starterbuttton
+  } else if ( course =="Desserts"){
+    dishes += dessertButton
+  } else {
+    dishes += entreebutton
+  }
 }
 
+for(let index = 0; index < dishes.length; index++) {
+
+  let dishes = dishes[index]
+
+    let dishItem = `<div class="individualPlates">
+                      <img src="${dishes.imageURL}"/>
+                      <div class="description">
+                        <h5>${dishes.title}</h5>
+                        <p>${dishes.description}</p>
+                      </div>
+                      <p class="price">${dishes.price}</p>
+                    </div>`
+              dishItem += completed
+              fullDishes.innerHTML = completed
 
 
-
-let btnFullMenu = document.querySelector("#btnFullMenu")
-btnFullMenu.addEventListener('click',function(){
- displayDishes(dishes)
-})
-
-let btnStarters = document.querySelector("#btnStarters")
-btnStarters.addEventListener('click',function(){
-
-
-
-})
-
-let btnEntrees = document.querySelector("#btnEntrees")
-btnEntrees.addEventListener('click',function(){
-
-
-
-})
-let btnDesserts = document.querySelector("#btnDesserts")
-btnDesserts.addEventListener('click',function(){
-
-})
+let starterbuttton = document.createEvenetlistner("click",starterDishesDisplay)
+let entreeButton = document.createEvenetlistner("click",starterDishesDisplay)
+let dessertButton = document.createEvenetlistner("click",starterDishesDisplay)
